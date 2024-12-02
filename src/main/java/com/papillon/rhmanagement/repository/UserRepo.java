0c417be9efc487c67141b784ac1af6ad9f20f443
DaoudAA <Ahmedaziz.daoud@gmail.com>
2024-12-02 16:43:11 +0100

    added jwt config security layer to back

---
+++ src/main/java/com/papillon/rhmanagement/repository/UserRepo.java
@@ -0,0 +1,11 @@
+package com.papillon.rhmanagement.repository;
+
+import com.papillon.rhmanagement.model.Users;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UserRepo extends JpaRepository<Users, Integer> {
+
+    Users findByUsername(String username);
+}
